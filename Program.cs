e519e7c8782fae9e776aae1d36c502ebc1414f77
Mazen Megahed <mazen.megahed1@outlook.com>
2025-06-30 23:39:51 +0300

    Question 2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@
             int s = int.Parse(Console.ReadLine());
             Console.WriteLine($"You have intered {s}");
             #endregion
+
+            #region Q2
+            //Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen
+            Console.WriteLine("Enter a number.");
+            int NUM = int.Parse(Console.ReadLine());
+            //it will give me exception error :The input string was not in a correct format.
+            #endregion
         }
     }
 }
