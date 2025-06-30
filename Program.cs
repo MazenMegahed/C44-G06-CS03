21a6378e573ec7eb4a902c92332873ff63b6b362
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-01 00:12:12 +0300

    Demo

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string demo = "I'm Eng.Mazen Megahed who had the approval from Eng.Ahmed to not submit the demo 👍🏻.";
         }
     }
 }
