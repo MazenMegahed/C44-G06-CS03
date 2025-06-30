0e58737c67722a9ecbde6d57824d46b0eea9e242
Lenovo <Lenovo@ADMINISTRATOR>
2025-06-30 23:29:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day03_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
